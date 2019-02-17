217c746f4dc2af40ea7256902226a2fd62fd3a97
Your Name <you@example.com>
2019-02-17 13:53:39 +0900

    メッセージ送信機能の非同期通信

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,48 @@
+$(function(){
+  function buildHTML(message){
+    var message_image = message.image ? message.image : ""
+    var html = `<div class="message">
+                  <div class="upper-message">
+                    <div class="upper-message__user--name">
+                       ${message.user_name}
+                    </div>
+                    <div class="upper-message__date">
+                       ${message.datetime}
+                    </div>
+                  </div>
+                  <div class="lower-message">
+                    <p class="lower-message__content">
+                      ${message.content}
+                    </p>
+                    <img class="lower-message__image" src="${message_image}">
+                  </div>
+                </div>`
+    return html;
+  }
+
+  $('.new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+     $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data) {
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.form__message, .form__mask__image').val('');
+      $('.chat-body').animate({scrollTop: $('.chat-body').get(0).scrollHeight}, 'slow');
+    })
+    .fail(function() {
+      alert('error');
+    })
+    .always(function(){
+      $('.form__submit').prop("disabled", false);
+    });
+  });
+});
